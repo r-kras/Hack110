91fb7095e80a42054fbdd13c580c7772b64e412f
rkras <rkras@ad.unc.edu>
2023-12-02 10:00:55 -0500

    Quick backup for game

--- website1/app.py
+++ website1/app.py
@@ -11,30 +11,21 @@ todo_count: int = 0
 def index():
     return render_template('index.html')
 
-@app.route('/create-todo', methods=["GET", "POST"])
-def create_todo():
-    if request.method == "POST":
-        global todo_list
-        global todo_count
-        
+@app.route('/guess_game', methods=["GET", "POST"])
+def create_guess_game():
+    if request.method == "POST":     
         title: str = request.form['title']
-        description: str = request.form['description']
-        color: str = request.form['color']
+        usr_guess: str = request.form['usr_guess']
  
         if title == '':
-            return render_template("create-todo.html")
+            return render_template("guess_game.html")
  
-        new_todo: todo = todo(todo_count, title, description, color)
+        new_todo: todo = todo(title, usr_guess)
         todo_list.append(new_todo)
  
-        todo_count += 1
- 
-        return render_template("success.html", title=title, description=description)
-    return render_template("create-todo.html")
+        return render_template("success.html", title=title, usr_guess=usr_guess)
+    return render_template("guess_game.html")
 
-@app.route('/view-todo-list')
-def view_todo_list():
-    return render_template('view-list.html', todo_list=todo_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
