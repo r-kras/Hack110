32094c1745696495e0b3f157fc63b7d0239cef6e
rkras <rkras@ad.unc.edu>
2023-12-02 00:03:16 -0500

    Finished with web_dev & made folder for web_dev

---
+++ web_dev/helpers.py
@@ -0,0 +1,10 @@
+# Task: Obtaining the Data
+class todo:
+    id: int
+    title: str
+    description: str
+ 
+    def __init__(self, id: int, title: str, description: str):
+        self.id = id
+        self.title = title
+        self.description = description
